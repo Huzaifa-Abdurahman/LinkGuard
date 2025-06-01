f90be49d6c99efd455294a84d06bfe6322e582e4
Huzaifa-Abdurahman <141252841+Huzaifa-Abdurahman@users.noreply.github.com>
2025-06-01 03:49:47 -0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app = FastAPI(title="Malicious URL Detector API", version="1.0.0")
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000","https://link-guard-rust.vercel.app", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
